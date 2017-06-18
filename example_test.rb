2bd3fc1999627c1ff75a1f3ba7c663522bbb32ac
Martin Becker <mbeckerwork@gmail.com>
2017-06-18 09:53:35 +0100

    updates example file

--- example_test.rb
+++ example_test.rb
@@ -1,7 +1,14 @@
 require_relative 'lib/pin_repin_client.rb'
 
+def pbcopy(input)
+  str = input.to_s
+  IO.popen('pbcopy', 'w') { |f| f << str }
+  str
+end
 #new login
 client = Pin::Client.login('username','password')
+client = Pin::Client.login('username','password',nil,"123.123.123.123:800","123.123.123.123:800")
+client = Pin::Client.login('username','password',nil,["123.123.123.123:800","123.123.123.123:800"])
 #store these values (in cookies, session, etc) for use in later requests
 # login_cookies = client.login_cookies
 # username = client.username
@@ -11,7 +18,7 @@ client = Pin::Client.login('username','password')
 # client.unfollow_user('sqearlworld', '796081809040378011')
 # client.follow_board('sqearlworld','chairs', '796081740320928279')
 # client.unfollow_board('sqearlworld','chairs', '796081740320928279')
-
+# client.followers('aureliencassegr').to_json
 #most recent pins
 # puts client.get_recent_pins('-cool-guitars')[2]
 # puts client.get_recent_pins('-cool-guitars','otherusername')[2]
